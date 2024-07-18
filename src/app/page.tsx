c2f5d45f58e2cce106f207110994955d21961dd4
alexander.zaykowski <alexander.zaykowski@hws.edu>
2024-07-18 16:12:20 -0400

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div className="z-50 flex min-h-screen flex-col items-center gap-10 bg-slate-50 p-10 md:flex-row md:items-baseline md:justify-between md:gap-0">
       <div className="z-50 flex h-[300px] w-[350px] flex-col justify-between rounded bg-blue-600 p-3 text-white shadow-xl shadow-slate-400 md:w-[600px] md:p-5">
-        <h1 className="font-bold md:text-2xl">Hi, I&apos;m Alex</h1>
+        <h1 className="text-2xl font-bold">Hi, I&apos;m Alex</h1>
         <p className="text-sm md:text-base">
           I&apos;m a Software Developer with practical experience building
           fullstack web applications along with the infrastructure to support
